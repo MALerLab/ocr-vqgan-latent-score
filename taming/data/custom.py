98816d681a9f99d4da8e51695f253f8d88eefe98
sakemin <sakemin.jung@gmail.com>
2024-08-04 22:22:34 +0900

    edit: grayscale mode added

--- taming/data/custom.py
+++ taming/data/custom.py
@@ -20,18 +20,18 @@ class CustomBase(Dataset):
 
 
 class CustomTrain(CustomBase):
-    def __init__(self, size, training_images_list_file, random_crop=True, augment=True):
+    def __init__(self, size, training_images_list_file, random_crop=True, augment=True, gray=True):
         super().__init__()
         with open(training_images_list_file, "r") as f:
             paths = f.read().splitlines()
-        self.data = ImagePaths(paths=paths, size=size, random_crop=random_crop, augment=augment)
+        self.data = ImagePaths(paths=paths, size=size, random_crop=random_crop, augment=augment, gray=gray)
 
 
 class CustomTest(CustomBase):
-    def __init__(self, size, test_images_list_file, random_crop=False, augment=False):
+    def __init__(self, size, test_images_list_file, random_crop=False, augment=False, gray=True):
         super().__init__()
         with open(test_images_list_file, "r") as f:
             paths = f.read().splitlines()
-        self.data = ImagePaths(paths=paths, size=size, random_crop=random_crop, augment=augment)
+        self.data = ImagePaths(paths=paths, size=size, random_crop=random_crop, augment=augment, gray=gray)
 
 
